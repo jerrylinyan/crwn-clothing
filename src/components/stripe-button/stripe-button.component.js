24e6c91559c79dec89a28ec191fd1ecbb150bacd
Andrew <andrew@Andrews-MacBook-Pro.local>
2020-08-03 14:27:42 +1000

    update stripe button connect to backend

--- src/components/stripe-button/stripe-button.component.js
+++ src/components/stripe-button/stripe-button.component.js
@@ -1,15 +1,35 @@
+import axios from "axios";
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import "./stripe-button.styles.scss";
 
 const StripeCheckoutButton = ({ price }) => {
+  const currency = "aud";
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51Gt6q6GbwhL4ssjAOmhexPixnGnyCtHZ1gpN49BLnps0JgqDVCo61q4xDdGrnfbSCqS3szmJJb65ZUMqaX8lQ8yh00pLZxDlsY";
 
-  const onToekn = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+  const onToken = (token) => {
+    // console.log(token);
+    // alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+        currency: currency,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful", response);
+      })
+      .catch((error) => {
+        console.log("Payment error:", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you provided credit card."
+        );
+      });
   };
 
   return (
@@ -22,7 +42,8 @@ const StripeCheckoutButton = ({ price }) => {
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
-      token={onToekn}
+      token={onToken}
+      currency={currency}
       stripeKey={publishableKey}
     />
   );
